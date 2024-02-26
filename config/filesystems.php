f3e42485545a9ad342c74a3925ee0dc51b5c33b4
vandewa <devandewananta@gmail.com>
2024-02-26 09:05:41 +0700

    prefix gcs

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
@@ -75,7 +75,7 @@ return [
                 "universe_domain" => "googleapis.com",
             ], // optional: Array of data that substitutes the .json file (see below)
             'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'web_opd'),
-            // 'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', 'gcs'), // optional: /default/path/to/apply/in/bucket
+            'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', 'ats'), // optional: /default/path/to/apply/in/bucket
             'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null), // see: Public URLs below
             'apiEndpoint' => env('GOOGLE_CLOUD_STORAGE_API_ENDPOINT', null), // set storageClient apiEndpoint
             'visibility' => 'noPredefinedVisibility', // optional: public|private
